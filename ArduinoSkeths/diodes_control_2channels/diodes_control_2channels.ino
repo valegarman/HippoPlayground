308319ee39f054fea47bbaae9bb59117dfe202a7
Pablo Abad Perez <pab9525@gmail.com>
2022-04-07 14:12:55 -0400

    Added bonsai files

--- ArduinoSkeths/diodes_control_2channels/diodes_control_2channels.ino
+++ ArduinoSkeths/diodes_control_2channels/diodes_control_2channels.ino
@@ -1,7 +1,9 @@
 /* Send a TTL pulse on any of the 2 trigger-in channel of the diodesBoard_2ch for 
 / controlling the diodes, according to some specification */
 // settings
-int modeOfOperation = 1; // 1 is tagging, 2 is continuous-triggered stimulation with only one pulse duration, 3 is continuous-triggered stimulation with one pulse duration for 2 diodes run independently
+//int modeOfOperation = 1; // 1 is tagging, 2 is continuous-triggered stimulation with only one pulse duration, 3 is continuous-triggered stimulation with one pulse duration for 2 diodes run independently
+
+int modeOfOperation = 0; // 1 is continuous-triggered stimulation with one pulse duration for 2 diodes run independently, 2 is continuous-triggered stimulation with only one pulse duration and 3 is tagging
 int delayBetweenPulsesFactor = 14; // factor that multiply pulses duration to compute delay
 int randomness = 6; // factor that multiply pulses duraction to compute randomness
 int pulseDuration[] = {5, 20, 50, 100}; // arary of duractions in ms, for example {5 20 50 100}
@@ -12,10 +14,30 @@ int pulseChoiceForStimulation = 2; // array position of the pulse that will be u
 int shortFactor = 3; // if pulses less than 10ms, multiply delayBetweenPulsesFactor by this
 int runChirp = 20; // run chirp protocol. Scalar will indicate number of repetition per pulse.
 
+
+// Button Values and LED display values for selection of mode of operation
+int buttonValue;
+int actualButtonValue = 0;
+int lastButtonValue = 0;
+int buttonCount = 0;
+int difButton;
+int maxButtonCount = 3;
+int interval = 0;
+int previousInterval = 0;
+unsigned long previousButtonMillis = 0;
+unsigned long currentButtonMillis;
+int ledState = LOW;
+
+
+
 // Pins
 const int diodesPinA = 2;
 const int diodesPinB = 3;
 const int activationPin = 12; // From the control behaviour arduino
+const int LED = LED_BUILTIN; // For displaying the mode of operation
+const int Button = 4; // For selecting the mode of operation
+
+
 
 // state variables
 int stimBlock = 0; // 0 is no stimulating
@@ -35,7 +57,10 @@ int increasedPeriodFactor;
 
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  //pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(Button,INPUT);
   
   pinMode(diodesPinA, OUTPUT);
   pinMode(diodesPinB, OUTPUT);
@@ -50,9 +75,66 @@ void setup() {
 }
 
 void loop() {
-  if (modeOfOperation == 1){
+
+
+  actualButtonValue = digitalRead(Button);
+  difButton = actualButtonValue - lastButtonValue;
+  lastButtonValue = actualButtonValue;
+  currentButtonMillis = millis();
+  if (difButton == -1){
+    buttonCount = buttonCount + 1;
+    }
+  if (buttonCount > maxButtonCount){
+    buttonCount = 0;
+    }
+  Serial.print("buttonCount: ");
+  Serial.println(buttonCount);
+  if (buttonCount == 0){
+    interval = 0;
+    digitalWrite(LED,HIGH);  
+    modeOfOperation = buttonCount;
+  }else if (buttonCount == 1){
+    digitalWrite(LED,HIGH);
+    interval = 100;
+    modeOfOperation = buttonCount;
+  } else if (buttonCount == 2){
+    digitalWrite(LED,HIGH);
+    interval = 500;
+    modeOfOperation = buttonCount;
+    } else if (buttonCount == 3){
+      digitalWrite(LED,HIGH);
+    interval = 1000;
+    modeOfOperation = buttonCount;
+    }
+
+  if (currentButtonMillis - previousButtonMillis >= interval){
+    // save the last time blinked the LED
+    previousButtonMillis = millis();
+
+    // if the LED is off turn it on and vice-versa
+    if (ledState == LOW){
+      ledState = HIGH;
+    }else{
+      ledState = LOW;
+        }
+        digitalWrite(LED,ledState);
+    }
+
+    
+  if (modeOfOperation == 3){  
+    Serial.println("3"); 
     if (digitalRead(activationPin) == HIGH | continousStimulation){ // if activated  or Continous Stimulation mode
       for (int i = 0; i < nPulsesDuration; ++i){
+        actualButtonValue = digitalRead(Button);
+        difButton = actualButtonValue - lastButtonValue;
+        lastButtonValue = actualButtonValue;
+        currentButtonMillis = millis();
+        if (difButton == -1){
+          buttonCount = buttonCount + 1;
+          modeOfOperation = buttonCount;
+          break;
+          Serial.println("AAAA");
+          }
         digitalWrite(diodesPinA, LOW);   // turn the diodes off
         digitalWrite(diodesPinB, LOW);   // turn the diodes off
         delay(1000);
@@ -156,10 +238,20 @@ void loop() {
           }
         }
       }
-
-      modeOfOperation = 3;
+      buttonCount = 0;
+      modeOfOperation = buttonCount;
+      
     }
-  } else if (modeOfOperation == 3){
+  } else if (modeOfOperation == 1){
+      Serial.println("1");
+      actualButtonValue = digitalRead(Button);
+      difButton = actualButtonValue - lastButtonValue;
+      lastButtonValue = actualButtonValue;
+      currentButtonMillis = millis();
+      if (difButton == -1){
+        buttonCount = buttonCount + 1;
+        modeOfOperation = buttonCount;
+      }
       increasedPeriodFactor = 1;
       pulse = pulseDuration[pulseChoiceForStimulation];
       delayFrom = (delayBetweenPulsesFactor - randomness) * pulse * increasedPeriodFactor;
@@ -197,8 +289,17 @@ void loop() {
                 digitalWrite(diodesPinB, diodeBState);    // Update the actual LED
               }
           }
-        modeOfOperation = 4;
+        //modeOfOperation = 4;
   } else if (modeOfOperation == 2){
+    Serial.println("2");
+    actualButtonValue = digitalRead(Button);
+    difButton = actualButtonValue - lastButtonValue;
+    lastButtonValue = actualButtonValue;
+    currentButtonMillis = millis();
+    if (difButton == -1){
+        buttonCount = buttonCount + 1;
+        modeOfOperation = buttonCount;
+      }
     if (digitalRead(activationPin) == HIGH){ // if activated
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on   
       digitalWrite(diodesPinA, HIGH);   // turn the diodes on
